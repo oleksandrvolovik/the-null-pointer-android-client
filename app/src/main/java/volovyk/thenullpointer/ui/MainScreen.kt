46148b478444cd313e832cccf04757e0bb71a0ee
Oleksandr <olieksandr.volovik@gmail.com>
2023-11-08 18:52:49 +0200

    fix: Fix UploadFileFAB appearing in the bottom left corner instead of the bottom right corner when FileList is empty

--- app/src/main/java/volovyk/thenullpointer/ui/MainScreen.kt
+++ app/src/main/java/volovyk/thenullpointer/ui/MainScreen.kt
@@ -3,6 +3,7 @@ package volovyk.thenullpointer.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,7 +26,7 @@ fun MainScreen(
     onFileUploadStateClick: (FileUploadState) -> Unit
 ) {
     Column(verticalArrangement = Arrangement.SpaceBetween) {
-        Column(horizontalAlignment = Alignment.End, modifier = Modifier.weight(1f)) {
+        Column(horizontalAlignment = Alignment.End, modifier = Modifier.weight(1f).fillMaxWidth()) {
             // Display files
             Box(modifier = Modifier.weight(1f)) {
                 FileList(
